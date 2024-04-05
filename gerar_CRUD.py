1fbf8d1608a34ee44f3df840564e3be171067746
vitorueno <vitoruenootto@gmail.com>
2024-04-05 16:41:47 -0300

    refactor: refatorando código para ficar mais clean

--- gerar_CRUD.py
+++ gerar_CRUD.py
@@ -2,136 +2,209 @@
 import mysql.connector
 from mysql.connector import errorcode
 
+# TODO - fazer em MariaDB -> só vai mudar a conexão (acho)
 
-def gera_conexao(host, user, password, database):
-    try:
-        db_connection = mysql.connector.connect(host = host, user = user, password = password, database = database)
-        print("Database connection made!")
-        return db_connection
-    except mysql.connector.Error as error:
-        if error.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database doesn't exist")
-        elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("User name or password is wrong")
-        else:
-            print(error)
-    else:
-        db_connection.close()
-
-def criar_procedure(conexao, operacao, nome_tabela):
-    print(f"Criando {operacao}")
-    #Selecionar as informações das metacolunas
-    cursor = conexao.cursor()
-    select = f"select * from information_schema.columns where table_schema = database() and table_name = '{nome_tabela}'"
-    cursor.execute(select)
-
-     # Listas das informações necessarias
-    nome_coluna = []
-    padrao_coluna = []
-    tipo_dado = []
-    is_nullable = []
-    chave_coluna = []
-    auto_increment = []
-
-    #Recebe todas as informações da metatabela
-    for coluna_info in cursor:
-        nome_coluna.append(coluna_info[3])
-        padrao_coluna.append(coluna_info[5])
-        tipo_dado.append(coluna_info[15])
-        is_nullable.append(coluna_info[6])
-        chave_coluna.append(coluna_info[16])
-        auto_increment.append(coluna_info[17])
-
-    ultimo = len(nome_coluna) - 1
-    qtd_colunas = len(nome_coluna)
-    nome_pk = ''
-    is_pk_auto_increment = False
-
-    # print(nome_coluna)
-    # print(padrao_coluna)
-    # print(tipo_dado)
-    # print(is_nullable)
-    # print(chave_coluna)
-    # print(auto_increment)
-    
-    drop_if_exists = f"drop procedure if exists {operacao}_{nome_tabela}"
-    cursor.execute(drop_if_exists)
-
-    procedure = f'create procedure {operacao}_{nome_tabela}('
-
-     
-
-    #Parte padrão de todas as procedures
-    for i in range(qtd_colunas):
-        if chave_coluna[i] == 'PRI':
-            nome_pk = nome_coluna[i]
-            is_pk_auto_increment = auto_increment[i] == 'auto_increment'
-
-    for i in range(qtd_colunas):
-        #TODO: VERIFICAR ONDE PRECISA ESSA LOGICA DO AUTO INCREMENT
-        if nome_coluna[i] == nome_pk and is_pk_auto_increment:
-            continue
-        procedure += f'in p_{nome_coluna[i]} {tipo_dado[i]}'    
-        if (i != ultimo):
-            procedure += ', '
-    procedure += ') begin '
-    
-    #Procedure Insert
-    if operacao == 'insert':
+class Gerador_Crud:    
+    def __init__(self, nome_tabela, host, user, password, database):
+        self.conexao = self.__gera_conexao(host, user, password, database)
+        self.nome_tabela = nome_tabela
         
-        procedure += f'insert into {nome_tabela} (' 
+        self.nome_coluna = []
+        self.padrao_coluna = []
+        self.tipo_dado = []
+        self.is_nullable = []
+        self.chave_coluna = []
+        self.auto_increment = []
+
+        self.cursor = None
+        self.__get_meta_dados()
+        
+        self.operacao = ''    
+        
+        self.nome_pk = ''
+        self.is_pk_auto_increment = False
+        self.__check_for_pk()
+        
+        self.ultimo = len(self.nome_coluna) - 1
+        self.qtd_colunas = len(self.nome_coluna)
+        
+        self.procedure = ''
+
+    def __gera_conexao(self, host, user, password, database):
+        try:
+            db_connection = mysql.connector.connect(host = host, user = user, password = password, database = database)
+            print("Database connection made!")
+            return db_connection
+        except mysql.connector.Error as error:
+            if error.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database doesn't exist")
+            elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("User name or password is wrong")
+            else:
+                print(error)
+        else:
+            db_connection.close()
+
+    def __check_for_pk(self):
+        qtd_colunas = len(self.nome_coluna)
+        
         for i in range(qtd_colunas):
-            if chave_coluna[i] == 'PRI' and auto_increment[i] == 'auto_increment':
+            if self.chave_coluna[i] == 'PRI':
+                self.nome_pk = self.nome_coluna[i]
+                self.is_pk_auto_increment = self.auto_increment[i] == 'auto_increment'
+      
+    def __get_meta_dados(self):
+        self.cursor = self.conexao.cursor()
+        select = f"select * from information_schema.columns where table_schema = database() and table_name = '{self.nome_tabela}'"
+        self.cursor.execute(select)
+
+        #Recebe todas as informações da metatabela
+        for coluna_info in self.cursor:
+            self.nome_coluna.append(coluna_info[3])
+            self.padrao_coluna.append(coluna_info[5])
+            self.tipo_dado.append(coluna_info[15])
+            self.is_nullable.append(coluna_info[6])
+            self.chave_coluna.append(coluna_info[16])
+            self.auto_increment.append(coluna_info[17])
+        
+    def __drop_if_exists(self):
+        drop_if_exists = f"drop procedure if exists {self.operacao}_{self.nome_tabela}"
+        self.cursor.execute(drop_if_exists)
+
+    def __create_procedure_no_id(self):
+        self.procedure = f'create procedure {self.operacao}_{self.nome_tabela}('
+
+        for i in range(self.qtd_colunas):
+            if self.nome_coluna[i] == self.nome_pk and self.is_pk_auto_increment:
                 continue
-            procedure += f'{nome_coluna[i]}' 
-            if (i != ultimo):
-                procedure += ', '
             
-        procedure += ') values ('
-
-        for i in range(qtd_colunas):
-            if nome_coluna[i] == nome_pk and is_pk_auto_increment:
-                continue
-            procedure += f'p_{nome_coluna[i]}'
-            if (i != ultimo):
-                procedure += ', '
-        procedure += '); '
-
-    #Procedure Update
-    elif operacao == 'update':
-        procedure += f'update {nome_tabela} set ' 
-        for i in range(qtd_colunas):
-            if chave_coluna[i] == 'PRI' and auto_increment[i] == 'auto_increment':
-                continue
-            procedure += f'{nome_coluna[i]} = p_{nome_coluna[i]}' 
-            if (i != ultimo):
-                procedure += ', '
-            
-        procedure += f' where {nome_pk} = p_{nome_pk}; '
-
-    #Procedure Delete
-    elif operacao == 'delete':
-        procedure += f'delete from {nome_tabela} where {nome_pk} = p_{nome_pk}; '        
-
-    #Procedure Select
-    elif operacao == 'select': 
-        procedure += f'select * from {nome_tabela} where {nome_pk} = p_{nome_pk}; '
-
-    procedure += 'end'
-    print(procedure)
+            self.procedure += f'in p_{self.nome_coluna[i]} {self.tipo_dado[i]}'    
+            if (i != self.ultimo):
+                self.procedure += ', '
+        self.procedure += ') begin '
     
-    cursor.execute(procedure)
-    print(f"{operacao} criada\n") 
+    def __create_procedure_only_id(self):
+        self.procedure = f'create procedure {self.operacao}_{self.nome_tabela}('
 
+        for i in range(self.qtd_colunas):
+            if self.nome_coluna[i] == self.nome_pk:
+                self.procedure += f'in p_{self.nome_coluna[i]} {self.tipo_dado[i]}'    
+          
+        self.procedure += ') begin '
+    
+    def __create_procedure_pk_and_fields(self):
+        self.procedure = f'create procedure {self.operacao}_{self.nome_tabela}('
 
-    # COLUMN_NAME[3], IS_NULLABLE [6], DATA_TYPE[7], COLUMN_KEY[16]
-        # talvez COLUMN_DEFAULT [5]
+        for i in range(self.qtd_colunas): 
+            self.procedure += f'in p_{self.nome_coluna[i]} {self.tipo_dado[i]}'    
+            if (i != self.ultimo):
+                self.procedure += ', '
+        self.procedure += ') begin '
+    
+    def criar_procedure_insert(self):
+        self.operacao = 'insert'
+        print(f"Criando {self.operacao}")
+        #Selecionar as informações das metacolunas
 
-def criar_CRUD(conexao, nome_tabela):
-    criar_procedure(conexao, 'insert', nome_tabela)
-    criar_procedure(conexao, 'update', nome_tabela)
-    criar_procedure(conexao, 'delete', nome_tabela)
-    criar_procedure(conexao, 'select', nome_tabela)
+        self.__drop_if_exists()
+        
+        self.__create_procedure_no_id()
+        
+        self.procedure += f'insert into {self.nome_tabela} (' 
+        for i in range(self.qtd_colunas):
+            if self.nome_coluna[i] == self.nome_pk and self.is_pk_auto_increment:
+                continue
+            
+            self.procedure += f'{self.nome_coluna[i]}' 
+            if (i != self.ultimo):
+                self.procedure += ', '
+            
+        self.procedure += ') values ('
+
+        for i in range(self.qtd_colunas):
+            if self.nome_coluna[i] == self.nome_pk and self.is_pk_auto_increment:
+                continue
+            
+            self.procedure += f'p_{self.nome_coluna[i]}'
+            if (i != self.ultimo):
+                self.procedure += ', '
+        self.procedure += '); '
+
+        self.procedure += 'end'
+        print(self.procedure)
+        
+        self.cursor.execute(self.procedure)
+        print(f"{self.operacao} criada\n") 
+        
+        return self.procedure
+
+    def criar_procedure_update(self):
+        self.operacao = 'update'
+        
+        print(f"Criando {self.operacao}")
+        
+        self.__drop_if_exists()
+
+        self.__create_procedure_pk_and_fields()
+        
+        self.procedure += f'update {self.nome_tabela} set ' 
+        for i in range(self.qtd_colunas):
+            self.procedure += f'{self.nome_coluna[i]} = p_{self.nome_coluna[i]}' 
+            if (i != self.ultimo):
+                self.procedure += ', '
+            
+        self.procedure += f' where {self.nome_pk} = p_{self.nome_pk}; '
+
+        self.procedure += 'end'
+        print(self.procedure)
+        
+        self.cursor.execute(self.procedure)
+        print(f"{self.operacao} criada\n") 
+        
+        return self.procedure
+           
+    def criar_procedure_delete(self):
+        self.operacao = 'delete'
+        
+        print(f"Criando {self.operacao}")
+        
+        self.__drop_if_exists()
+
+        self.__create_procedure_only_id()
+    
+        self.procedure += f'delete from {self.nome_tabela} where {self.nome_pk} = p_{self.nome_pk}; '        
+
+        self.procedure += 'end'
+        print(self.procedure)
+        
+        self.cursor.execute(self.procedure)
+        print(f"{self.operacao} criada\n") 
+        
+        return self.procedure
+
+    def criar_procedure_select(self):
+        self.operacao = 'select'
+        print(f"Criando {self.operacao}")
+        
+        self.__drop_if_exists()
+        
+        self.__create_procedure_only_id()
+
+        self.procedure += f'select * from {self.nome_tabela} where {self.nome_pk} = p_{self.nome_pk}; '
+
+        self.procedure += 'end'
+        print(self.procedure)
+        
+        self.cursor.execute(self.procedure)
+        print(f"{self.operacao} criada\n")
+        
+        return self.procedure
+
+    def criar_CRUD(self):
+        self.criar_procedure_insert()
+        self.criar_procedure_update()
+        self.criar_procedure_delete()
+        self.criar_procedure_select()
 
 if __name__ == '__main__':
     # Conexão com o BD
@@ -140,8 +213,8 @@ if __name__ == '__main__':
     password = 'root'
     database ='animais'
     tabela = 'gato'
-    conexao = gera_conexao(host, user, password, database)
 
-    #Gera o CRUD da tabela
-    criar_CRUD(conexao, tabela)
+    crud = Gerador_Crud(tabela, host, user, password, database) 
+
+    crud.criar_CRUD()
     
